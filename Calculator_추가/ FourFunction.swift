f9fb643cbbe9733815ee891ad4cb0d2023d76490
new git <new git>
2024-06-10 18:42:02 +0900

    first commit

---
+++ Calculator_추가/ FourFunction.swift
@@ -0,0 +1,34 @@
+//
+//  FourFunctionCalculator.swift
+//  Calculator
+//
+//  Created by Leejuhee on 6/9/24.
+
+class Addition: Operation {
+    override func calculate(num1: Double, num2: Double) -> Double {
+        return num1 + num2
+    }
+}
+
+class Subtraction: Operation {
+    override func calculate(num1: Double, num2: Double) -> Double {
+        return num1 - num2
+    }
+}
+
+class Multiplication: Operation {
+    override func calculate(num1: Double, num2: Double) -> Double {
+        return num1 * num2
+    }
+}
+
+class Division: Operation {
+    override func calculate(num1: Double, num2: Double) -> Double {
+        if num2 != 0 {
+            return num1 / num2
+        } else {
+            print("Error: 0으로 나눌 수 없습니다!")
+            return 0.0
+        }
+    }
+}
---
+++ Calculator_추가/Operation.swift
@@ -0,0 +1,10 @@
+//
+//  Operation.swift
+//  Calculator_ 추가
+//
+//  Created by t2023-m0023 on 6/10/24.
+class Operation {
+    func calculate(num1: Double, num2: Double) -> Double {
+        return 0.0 // 기본 연산은 0을 반환
+    }
+}
